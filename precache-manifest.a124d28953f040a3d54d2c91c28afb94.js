e38893b64f70254508abceff12f86ab62e2dcd78
Mishari Adel Toama <mat@npstoday.com>
2020-10-21 01:14:38 +0200

    Updates

---
+++ precache-manifest.a124d28953f040a3d54d2c91c28afb94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fabf24054b2b525c2bafce44308ba0f",
+    "url": "/reviews/index.html"
+  },
+  {
+    "revision": "dd7ca35217e845e7f4d9",
+    "url": "/reviews/static/css/main.65648557.chunk.css"
+  },
+  {
+    "revision": "c5047ad9f24df2e8757b",
+    "url": "/reviews/static/js/2.b2d387ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reviews/static/js/2.b2d387ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7ca35217e845e7f4d9",
+    "url": "/reviews/static/js/main.86f26bae.chunk.js"
+  },
+  {
+    "revision": "a2d56b1e38d321c84a5f",
+    "url": "/reviews/static/js/runtime-main.5e67ea47.js"
+  },
+  {
+    "revision": "f6b0543db51e83396515626c8c97a8d2",
+    "url": "/reviews/static/media/bg.f6b0543d.jpg"
+  },
+  {
+    "revision": "ee2c72c094374d9c2e9dafdc69deb24b",
+    "url": "/reviews/static/media/footer-logo.ee2c72c0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.141a4ded981e54c319670c201282304d.js"
+  "/reviews/precache-manifest.a124d28953f040a3d54d2c91c28afb94.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reviews/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
